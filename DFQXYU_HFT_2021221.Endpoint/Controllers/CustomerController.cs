91deb288892deceb119e8d62ae3f329b7c63cbc8
laszlo-balogh <balaszlo444@gmail.com>
2021-11-24 15:43:08 +0100

    finish the 3 basic controller classes

--- DFQXYU_HFT_2021221.Endpoint/Controllers/CustomerController.cs
+++ DFQXYU_HFT_2021221.Endpoint/Controllers/CustomerController.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using DFQXYU_HFT_2021221.Logic;
+using DFQXYU_HFT_2021221.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DFQXYU_HFT_2021221.Endpoint.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class CustomerController
     {
+        ICustomerLogic customerLogic;
+        public CustomerController(ICustomerLogic customerLogic)
+        {
+            this.customerLogic = customerLogic;
+        }
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return customerLogic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Customer Get(int id)
+        {
+            return customerLogic.Read(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Customer customer)
+        {
+            customerLogic.Create(customer);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Customer customer)
+        {
+            customerLogic.Update(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            customerLogic.Delete(id);
+        }
     }
 }
--- DFQXYU_HFT_2021221.Endpoint/Controllers/MovieRentalController.cs
+++ DFQXYU_HFT_2021221.Endpoint/Controllers/MovieRentalController.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using DFQXYU_HFT_2021221.Logic;
+using DFQXYU_HFT_2021221.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DFQXYU_HFT_2021221.Endpoint.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class MovieRentalController
     {
+        IMovieReantalLogic rentalLogic;
+        public MovieRentalController(IMovieReantalLogic rentalLogic)
+        {
+            this.rentalLogic = rentalLogic;
+        }
+        [HttpGet]
+        public IEnumerable<MovieRental> Get()
+        {
+            return rentalLogic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public MovieRental Get(int id)
+        {
+            return rentalLogic.Read(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] MovieRental rental)
+        {
+            rentalLogic.Create(rental);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] MovieRental rental)
+        {
+            rentalLogic.Update(rental);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            rentalLogic.Delete(id);
+        }
     }
 }
