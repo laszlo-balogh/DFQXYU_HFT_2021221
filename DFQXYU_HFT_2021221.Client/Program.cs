d7e05f9cc03a6f21d306a2e6a711cfa54889b254
laszlo-balogh <balaszlo444@gmail.com>
2021-11-24 18:20:10 +0100

    modify the dependencies

--- DFQXYU_HFT_2021221.Client/Program.cs
+++ DFQXYU_HFT_2021221.Client/Program.cs
@@ -1,6 +1,4 @@
-﻿using DFQXYU_HFT_2021221.Data;
-using DFQXYU_HFT_2021221.Logic;
-using DFQXYU_HFT_2021221.Repository;
+﻿using DFQXYU_HFT_2021221.Models;
 using System;
 using System.Linq;
 
@@ -10,25 +8,16 @@ namespace DFQXYU_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("Hello World!");
             //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DFQXYU_HFT_2021221_Database.mdf;Integrated Security=True
 
-            MovieDbContext db = new MovieDbContext();
 
-             db.Movies.ToList().ForEach(x => Console.WriteLine($"\t{x.MovieTitle}"));
+            RestService restService = new RestService("http://localhost:47417/");
 
-            IMovieReantalLogic ml = new MovieRentalLogic(new MovieRentalRepository(db), new MovieRepository(db), new CustomerRepository(db));
+            var movies = restService.Get<Movie>("movies");
+            var customers = restService.Get<Customer>("customers");
+            var rentals = restService.Get<MovieRental>("rentals");
 
-            var var1 = ml.RentalsWithBefore2000();
-            var var2 = ml.RentalsWithIsRegularCustomer();
-            var var3 = ml.RentalsWithNotJamesCameronAndCustomerBornDateIs2000();
-            var var4 = ml.RentalsWithJamesCameronMovies();
-            var var5 = ml.RentalsByCustomerNames();           
-
-            ;
-
-
-
+            //var movie = restService.Get<>
 
 
         }
