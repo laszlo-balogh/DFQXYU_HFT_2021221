a3cd4709a00e1e809e925561fbd825ea7ac9af11
laszlo-balogh <balaszlo444@gmail.com>
2021-11-29 21:11:50 +0100

    change/delete attributes

--- DFQXYU_HFT_2021221.Models/Customer.cs
+++ DFQXYU_HFT_2021221.Models/Customer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DFQXYU_HFT_2021221.Models
@@ -14,22 +15,23 @@ namespace DFQXYU_HFT_2021221.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CustomerID { get; set; }
-        [Required]
+        //[Required]
         public string Name { get; set; }
         [Required]
         public DateTime BornDate { get; set; }
-        [Required]
+        //[Required]
         public string Email { get; set; }
         [Required]
-        [MaxLength(9)]
-        [MinLength(9)]
+        //[MaxLength(9)]
+        //[MinLength(9)]
         public int PhoneNumber { get; set; }
+        [JsonIgnore]
         [NotMapped]
         public virtual ICollection<MovieRental> Rentals { get; set; }
         public bool RegularCustomer { get; set; }
         public Customer()
         {
-            this.Rentals = new HashSet<MovieRental>();
+            //this.Rentals = new HashSet<MovieRental>();
             this.RegularCustomer = false;
         }
 
--- DFQXYU_HFT_2021221.Models/Movie.cs
+++ DFQXYU_HFT_2021221.Models/Movie.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DFQXYU_HFT_2021221.Models
@@ -16,18 +17,19 @@ namespace DFQXYU_HFT_2021221.Models
         public int MovieID { get; set; }
         [Required]
         public string MovieTitle { get; set; }
-        [MaxLength(4)]
+        //[MaxLength(4)]
         public int Year { get; set; }
         public string Producer { get; set; }
         public string Location { get; set; }
         [Required]
         public int? Price { get; set; }
+        [JsonIgnore]
         [NotMapped]
         public virtual ICollection<MovieRental> Rentals { get; set; }
 
         public Movie()
         {
-            this.Rentals = new HashSet<MovieRental>();
+            //this.Rentals = new HashSet<MovieRental>();
         }
         public override string ToString()
         {
